01fddff2c9492d56fd2ced9c69bae01922727c66
Wyrine <kashahat@gmail.com>
2018-04-17 23:21:27 -0400

    added pseudocode for how to build hash table

--- driver.cpp
+++ driver.cpp
@@ -6,12 +6,35 @@ using namespace std;
 int
 main(int argc, const char* argv[])
 {
-		string names[2];
+		string fNames[2];
 		uint threadCount = thread::hardware_concurrency();
 		
 		for(int i = 1; i < argc; i++)
-				names[i-1] = string(argv[i]);
+				fNames[i-1] = string(argv[i]);
 		cout << threadCount << endl;
 
 		return 0;
 }
+
+/*
+1) Read each 'query' string from the main strand into memory
+		1) Create a vector of strings that are the normal sequence
+		2) Create a vector of strings that are the random sequence
+		3) Create the threads 
+				--Thread view --
+				1)Randomize the indices to look up
+				2)Sort them in ascending order
+				for(int i = 0; i < s1.length(); i++){
+						for(int j = 0; j < indices.length(); j++)
+						{
+								int c = convert(s1[i][indices[j]]);
+								//can probably do s2 here too
+								~~~~~~~check this next line, unsure about these offsets~~~~~~~
+								c = c << indices[indices.length() - j - 1] - 1;
+								//insert into hash table
+						}
+				}
+
+
+
+*/
--- helper.hpp
+++ helper.hpp
@@ -5,3 +5,4 @@
 #include <vector>
 
 typedef unsigned int uint;
+#define TABLES_TO_THREAD 1
